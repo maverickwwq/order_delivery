0040ea5f82f1287eba2a45582a07c95c83a49419
kimi wang <maverickwwq@gmail.com>
2020-04-01 09:44:41 +0800

    2020/04/01 first upload
    第一次上传

---
+++ CommUtil.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dispatch.BaseUtil
+{
+    class CommUtil
+    {
+        //通讯操作类型
+        public static readonly string COMM_TYPE_DOWN_ORDEROP = "DOWN_ORDEROP"; 
+
+        //机房代码定义
+        public static readonly string DEPT_JIA = "7830";//甲机房的机房代码
+        public static readonly string DEPT_YI = "7831";//乙机房的机房代码
+
+    }
+}
